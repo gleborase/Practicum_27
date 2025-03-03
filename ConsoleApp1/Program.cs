9b7d2f198c5dfe220638267a354df52877977119
gleborase <krasnopolsci@gmail.com>
2025-03-03 19:25:16 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{/// <summary>
+/// Задача 2. Заполнить одномерный массив случайным образом и выполнить сортировку по возрастанию (через вложенный цикл, с использованием метода Sort()).
+/// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] omas = new int[Input()];
+            Vvod(omas);
+            Vivod(omas);
+            Array.Sort(omas);
+            Vivod(omas);
+            Console.Read();
+        }
+        static int Input()
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        static void Vvod(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.Write($"arr[{i}]= ");
+                omas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        static void Vivod(int[] omas)
+        {
+            for (int i = 0; i < omas.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}]={omas[i]}");
+            }
+        }
+    }
+}
